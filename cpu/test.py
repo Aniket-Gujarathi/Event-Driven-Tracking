12ed4c4d1d441436c9c75fa6cd8f1f174deb7aa9
Aniket <aniketg.vnit@gmail.com>
2021-05-03 15:10:29 +0530

    correct test code

--- cpu/test.py
+++ cpu/test.py
@@ -1,18 +1,19 @@
 import cv2
 import numpy as np 
+from tqdm import tqdm
 
 def  calculate_likelihood(image):
     
     height, width, _ = image.shape
-    f = open("data_exp2.txt", "w")
+    f = open("data_exp_648.txt", "w")
     idx = np.where(np.any(image != [0, 0, 0], axis=-1)) # ids of events in the image
     
-    for x_c in range(0, width - 1, 10): # all possible Xc state
-        for y_c in range(0, height - 1, 10): # all possible Yc state
+    for x_c in tqdm(range(0, width - 1, 10)): # all possible Xc state
+        for y_c in tqdm(range(0, height - 1, 10)): # all possible Yc state
             for r in range(10, 50, 5): # all possible radius state
                 score = 0
                 likelihood = 3 # min likelihood
-                for i in range(0, idx[0].size, 10):
+                for i in range(0, idx[0].size):
                     vx = idx[1][i]
                     vy = idx[0][i]
 
@@ -35,13 +36,13 @@ def  calculate_likelihood(image):
                     else:
                         score -= 0.5
 
-                    # write the data to a txt file
-                    f.write(str(x_c) + " " + str(y_c) + " " + str(r) + " " + str(score) + " " + str(likelihood) + "\n")
+                # write the data to a txt file
+                f.write(str(x_c) + " " + str(y_c) + " " + str(r) + " " + str(score) + " " + str(likelihood) + "\n")
 
                         
 
 if __name__ == '__main__':
-    image = cv2.imread('/home/aniket/yarp-install/projects/particle-filter-tracking/cpu/test_img/00001217.jpg')
+    image = cv2.imread('/home/aniket/yarp-install/projects/particle-filter-tracking/cpu/test_img/00000648.jpg')
     calculate_likelihood(image)
     
     
