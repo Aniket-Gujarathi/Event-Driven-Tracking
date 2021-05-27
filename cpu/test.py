94fc2d83722a9f6d13bb82b1437d366c17a559db
Aniket <aniketg.vnit@gmail.com>
2021-05-27 12:35:09 +0530

    changes in test_debug code

--- cpu/test.py
+++ cpu/test.py
@@ -6,36 +6,38 @@ from sys import argv, exit
 def  calculate_likelihood(image):
     
     height, width, _ = image.shape
-    f = open("test_2.txt", "w")
+    f = open("test_648_26May.txt", "w")
     idx = np.where(np.any(image != [0, 0, 0], axis=-1)) # ids of events in the image
     
     for x_c in tqdm(range(0, width - 1, 5)): # all possible Xc state
         for y_c in tqdm(range(0, height - 1, 5)): # all possible Yc state
-            for r in range(10, 50, 5): # all possible radius state
+            for r in range(10, 40, 5): # all possible radius state
                 score = 0
                 likelihood = 3 # min likelihood
                 for i in range(0, idx[0].size):
                     vx = idx[1][i]
                     vy = idx[0][i]
 
-                    dist_circ = np.abs(np.sqrt((x_c - vx)**2 + (y_c - vy)**2) - r)                
-
+                    dist_circ = (np.sqrt((x_c - vx)**2 + (y_c - vy)**2) - r)                    # use real values 
+                                                                                                # dont penalize outside events, only interior
                     if(dist_circ > 2.0):
+                        score -= 0.1
                         continue   
 
-                    cval = 0.0
-                    if(dist_circ < 1.0):
+                    cval = None
+                    if(-2.0 <= dist_circ <= 2.0):
                         cval = 1.0
-                    elif(dist_circ < 2.0):
-                        cval = -0.5
-                    if(cval >= 0): 
-                        improve = cval
-                        if(improve > 0):
-                            score += improve
-                            if(score >= likelihood):
-                                likelihood = score    
-                    else:
-                        score -= 0.5
+                    elif(-3.0 <= dist_circ < -2.0):
+                        cval = -1.0
+                    if (cval):
+                        if(cval >= 0.0): 
+                            improve = cval
+                            if(improve > 0):
+                                score += improve
+                                if(score >= likelihood):
+                                    likelihood = score    
+                        elif (cval < 0.0):
+                            score -= 0.5
 
                 if (score != 0):
                     # write the data to a txt file
